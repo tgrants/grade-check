a7f9dc9063fb0d4dd34f7ea3bd961d61f05bb717
klappscheinwerfer <toms.grants@gmail.com>
2025-04-29 01:36:08 +0300

    Fix some pylint issues, improve initadmin, add env documentation, replace decouple

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,14 @@ urlpatterns = [
 	path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 	path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 	path('profile/', views.profile_view, name='profile'),
-	path('password_change/', auth_views.PasswordChangeView.as_view(template_name='password_change.html'), name='password_change'),
-	path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'), name='password_change_done'),
+	path(
+		'password_change/',
+		auth_views.PasswordChangeView.as_view(template_name='password_change.html'),
+		name='password_change'
+	),
+	path(
+		'password_change/done/',
+		auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'),
+		name='password_change_done'
+	),
 ]
